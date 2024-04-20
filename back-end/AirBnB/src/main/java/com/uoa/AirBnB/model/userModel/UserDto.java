39f032ea962e00af1573b95861f2ea88750418f1
shreyas-peddi <peddishreyas2@gmail.com>
2024-04-20 04:34:01 -0400

    Initial commit

---
+++ back-end/AirBnB/src/main/java/com/uoa/AirBnB/model/userModel/UserDto.java
@@ -0,0 +1,31 @@
+package com.uoa.AirBnB.model.userModel;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.lang.Nullable;
+import com.uoa.AirBnB.model.imageModel.ImageDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDto {
+
+    private long id;
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String number;
+
+    private Boolean approved;
+
+    private ImageDto image;
+}
